dcf62d262186711302cada931660580da61d8aac
dionannd <dionananda77@gmail.com>
2022-01-02 04:12:30 +0700

    fix: layout footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,37 +1,48 @@
 import * as React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Image, Stack, HStack, Link } from '@chakra-ui/react';
 
 export const Footer: React.FC = () => {
   return (
-    <Box
-      bg="primary.accent7"
-      w="full"
-      h="full"
-      px={{ base: '20px', lg: '12rem' }}
-      py={10}
-      color="white"
-      pt="15px"
-      pb="10px"
-    >
-      <Flex>
-        <Flex direction="column">
-          <Text fontWeight="semibold">Link</Text>
-          <Text fontSize="sm">Home</Text>
-          <Text fontSize="sm">Blog</Text>
+    <Box bg="primary.accent7" w="full" h="full" px={{ base: '20px', lg: '12rem' }}>
+      <Stack py={{ base: '10', lg: '20' }} direction={['column', 'row']} spacing={{ base: '2', sm: '5', lg: '10' }}>
+        <Flex direction="column" justifyContent="left">
+          <Text color="white" fontSize="14px" fontWeight="semibold">
+            Links
+          </Text>
+          <Link color="white" fontSize="13px" href="/" py="1">
+            Home
+          </Link>
+          <Link color="white" fontSize="13px" href="/category/studio">
+            Studio
+          </Link>
         </Flex>
         <Flex direction="column" mx="40px">
-          <Text fontWeight="semibold">Social Media</Text>
-          <Text fontSize="sm">Instagram</Text>
+          <Text fontWeight="semibold" color="white" fontSize="14px">
+            Social Media
+          </Text>
+          <Link href="https://www.instagram.com/fixcreativephotography/" isExternal color="white">
+            <HStack>
+              <Image src="/assets/sosmed/instagram.svg" alt="instagram" w="24px" />
+              <Text color="white" fontSize="13px">
+                Fixcreative
+              </Text>
+            </HStack>
+          </Link>
         </Flex>
         <Flex direction="column" mx="20px">
-          <Text fontWeight="semibold">Address</Text>
-          <Text fontSize="sm">whatsapp</Text>
-          <Text fontSize="sm">telepon</Text>
-          <Text fontSize="sm">fixcreative@gmail.com</Text>
+          <Text fontWeight="semibold" color="white" fontSize="14px">
+            Address
+          </Text>
+          <HStack>
+            <Image src="/assets/sosmed/whatsapp.svg" alt="" />
+            <Text fontSize="13px" color="white">
+              0813 1594 1338
+            </Text>
+          </HStack>
         </Flex>
-      </Flex>
-      <Text textAlign="center" mt="2em" fontSize="14px">
-        Powered by <b>Fixcreative</b>.
+      </Stack>
+      <Text textAlign="center" fontSize="14px" color="white" pb="10px">
+        &copy;{new Date().getFullYear()}. Powered by <b>Fixcreative</b>.
       </Text>
     </Box>
   );
