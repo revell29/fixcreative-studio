a5a2f9206f5a51bb41a5be5b187a82646107a100
apsya Dira <diraapsya0@gmail.com>
2022-01-02 05:38:29 +0700

    fix: review carousel

--- components/hero/review.tsx
+++ components/hero/review.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Avatar, Box, HStack, Text } from '@chakra-ui/react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Reviews } from '~/types/base';
 
 interface ReviewProps {
@@ -9,55 +10,91 @@ interface ReviewProps {
 }
 
 const ReviewHero: React.FC<ReviewProps> = ({ reviews }: ReviewProps) => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 600 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 500, min: 500 },
-      items: 1,
-    },
+  const settings: Settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 5,
+    useCSS: true,
+    className: 'slider',
+    nextArrow: (
+      <Box
+        h="40px"
+        w="40px"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        display="block"
+        rounded="full"
+        mr="4rem"
+      />
+    ),
+    prevArrow: (
+      <Box
+        h="40px"
+        w="40px"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        display="block"
+        rounded="full"
+        ml="2.5rem"
+        zIndex={1}
+      />
+    ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <Carousel
-      responsive={responsive}
-      swipeable={true}
-      draggable={true}
-      arrows={false}
-      autoPlay={true}
-      showDots={true}
-      infinite={true}
-      containerClass="carousel-container"
-      dotListClass="custom-dot-list-style"
-    >
-      {reviews &&
-        reviews.map((review) => (
-          <Box border="1px" borderColor="gray.300" p={4} key={review.id} m={2} rounded="md" maxH="300px">
-            <Text fontSize="14px" fontStyle="italic" noOfLines={4}>
-              “{review.attributes.review}”
-            </Text>
-            <Box mt={4}>
-              <HStack>
-                <Avatar name={review.attributes.customer_name} />
-                <Text fontSize="14px" fontWeight="600">
-                  {review.attributes.customer_name}
+    <Box mt={7}>
+      <Slider {...settings}>
+        {reviews &&
+          reviews.map((review) => (
+            <Box key={review.id}>
+              <Box border="1px" borderColor="gray.300" p={4} m={2} rounded="md" minH="220px" position="relative" mr={4}>
+                <Text fontSize="14px" fontStyle="italic" noOfLines={4}>
+                  “{review.attributes.review}”
                 </Text>
-              </HStack>
+                <Box mt={4} position="absolute" bottom={3}>
+                  <HStack>
+                    <Avatar name={review.attributes.customer_name} size="sm" />
+                    <Text fontSize="14px" fontWeight="600">
+                      {review.attributes.customer_name}
+                    </Text>
+                  </HStack>
+                </Box>
+              </Box>
             </Box>
-          </Box>
-        ))}
-    </Carousel>
+          ))}
+      </Slider>
+    </Box>
   );
 };
 
